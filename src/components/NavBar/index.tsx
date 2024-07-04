080e3220c12bc981b0e757efae18b2b3ecc33207
Houdaharit <houdaharit20@gmail.com>
2024-07-04 22:08:24 +0100

    init commit

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,25 @@
+const NavBar = () => {
+  return (
+    <div className="h-[73px] leading-5 font-bold flex justify-between p-6">
+      <section className="flex gap-6 items-center">
+        <div className="text-4xl">Logo</div>
+        <div>Notre Entreprise</div>
+        <div>Notre science</div>
+        <div>Votre Santé</div>
+        <div>Partenariat</div>
+      </section>
+      <section className="flex gap-6 items-center">
+        <div>Média</div>
+        <div>Investisseurs</div>
+        <div>Carrières</div>
+        <div>Partenariat Paris 2024</div>
+        <img src="/SearchIcon.svg" />
+        <section className="flex cursor-pointer gap-4">
+          <img src="/GlobeIcon.svg" />
+          <label>Global</label>
+        </section>
+      </section>
+    </div>
+  );
+};
+export default NavBar;
