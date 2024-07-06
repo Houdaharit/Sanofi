79948861c8ae461678aab8a000aa51f5d485f7dd
Houdaharit <houdaharit20@gmail.com>
2024-07-06 02:04:57 +0100

    Refactor: converted <div> to <Logo> component

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,10 @@
+import Logo from "../Logo";
+
 const NavBar = () => {
   return (
     <nav className="h-[73px] leading-5 text-[15px] font-bold flex justify-between p-6">
       <section className="flex gap-6 items-center">
-        <div className="text-3xl mr-3">Sanofi</div>
+        <Logo color="black"/>
         <div>Notre Entreprise</div>
         <div>Notre science</div>
         <div>Votre Santé</div>
@@ -13,7 +15,7 @@ const NavBar = () => {
         <div>Investisseurs</div>
         <div>Carrières</div>
         <div>Partenariat Paris 2024</div>
-        <img src="/SearchIcon.svg" alt="Search Icon"/>
+        <img src="/SearchIcon.svg" alt="Search Icon" />
         <section className="flex cursor-pointer gap-3">
           <img src="/GlobeIcon.svg" alt="Globe Icon" />
           <span>Global</span>
