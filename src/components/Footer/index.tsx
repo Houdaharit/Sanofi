05bb7f12768bcac53c07bda758021de39f2f819e
Houdaharit <houdaharit20@gmail.com>
2024-07-09 16:54:25 +0100

    STYLE: changed position from bottom-0 to nothing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import TopOfPage from "./TopOfPage";
 
 const Footer = () => {
   return (
-    <footer className="bg-black h-[88px] absolute bottom-0 w-full flex items-center mt-32">
+    <footer className="bg-black lg:h-[88px] w-full flex items-center mt-32">
       <div className="pl-8 pr-8 flex justify-between w-full">
         <Logo color="white" />
         <section className="flex gap-16">
