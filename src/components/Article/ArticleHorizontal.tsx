68759957a649c9f7e3857f4125802f42c1f0a8c5
Houdaharit <houdaharit20@gmail.com>
2024-07-10 00:41:32 +0100

    REWORD: changed Articale to Card dir

--- src/components/Article/ArticleHorizontal.tsx
+++
@@ -1,4 +0,0 @@
-const ArticleHorizontal = () => {
-  return <></>;
-};
-export default ArticleHorizontal;
--- src/components/Article/ArticleVertical.tsx
+++
@@ -1,5 +0,0 @@
-
-const ArticleVertical = () => {
-  return <></>;
-};
-export default ArticleVertical;
