01e3a1c1e754875de408448a5b2832380475d148
Houdaharit <houdaharit20@gmail.com>
2024-07-09 16:59:55 +0100

    REVISE: change div global to a button

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,21 +4,25 @@ const NavBar = () => {
   return (
     <nav className="h-[73px] leading-5 text-[15px] font-bold flex justify-between p-6">
       <section className="flex gap-6 items-center">
-        <Logo color="black"/>
+        <Logo color="black" />
         <div>Notre Entreprise</div>
         <div>Notre science</div>
         <div>Votre Santé</div>
         <div>Partenariat</div>
       </section>
+
       <section className="flex gap-6 items-center">
         <div>Média</div>
         <div>Investisseurs</div>
         <div>Carrières</div>
         <div>Partenariat Paris 2024</div>
         <img src="/SearchIcon.svg" alt="Search Icon" />
-        <section className="flex cursor-pointer gap-3">
-          <img src="/GlobeIcon.svg" alt="Globe Icon" />
-          <span>Global</span>
+
+        <section>
+          <button className="flex gap-2">
+            <img src="/GlobeIcon.svg" alt="Globe Icon" />
+            <span>Global</span>
+          </button>
         </section>
       </section>
     </nav>
