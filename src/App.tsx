57a70bd4e9031b81840137a96e19fbc3a765d767
Houdaharit <houdaharit20@gmail.com>
2024-07-13 00:40:39 +0100

    DELETE: empty App component because routes are now on main access pages using RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Layout from "./components/Layout";
-import Main from "./components/Main";
 
 function App() {
   return (
     <>
-      <Layout>
-        <Main />
-      </Layout>
     </>
   );
 }
