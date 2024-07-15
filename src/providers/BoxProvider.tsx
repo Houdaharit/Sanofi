329e7f4607d97c79e6e1c27752438a95a13e184f
Houdaharit <houdaharit20@gmail.com>
2024-07-15 02:03:25 +0100

    ADD: a context to pass the value of the top main box height

---
+++ src/providers/BoxProvider.tsx
@@ -0,0 +1,32 @@
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+interface BoxContextType {
+  boxHeight: number;
+  setBoxHeight: Dispatch<SetStateAction<number>>;
+}
+const BoxContext = createContext<BoxContextType | undefined>(undefined);
+
+const BoxProvider = ({ children }: { children: ReactNode }) => {
+  const [boxHeight, setBoxHeight] = useState<number>(0);
+  return (
+    <BoxContext.Provider value={{ boxHeight, setBoxHeight }}>
+      {children}
+    </BoxContext.Provider>
+  );
+};
+
+const useContextBox = () => {
+  const context = useContext(BoxContext);
+  if (context == undefined)
+    throw new Error("context must be used within an BoxProvider");
+  return context;
+};
+
+export { BoxProvider, useContextBox };
