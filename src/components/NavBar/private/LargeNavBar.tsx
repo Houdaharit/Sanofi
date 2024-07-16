6d8434c0764415576b88277d0873a0e65621c577
Houdaharit <houdaharit20@gmail.com>
2024-07-16 20:01:19 +0100

    FEAT: NavBar for large windows

---
+++ src/components/NavBar/private/LargeNavBar.tsx
@@ -0,0 +1,31 @@
+import Logo from "../../Logo";
+
+const LargeNavBar = () => {
+  return (
+    <nav id="haut de la page" className="h-[73px] leading-5 text-[15px] font-semibold flex justify-between p-6">
+      <section className="flex gap-6 items-center">
+        <Logo color="black" />
+        <div>Notre Entreprise</div>
+        <div>Notre science</div>
+        <div>Votre Santé</div>
+        <div>Partenariat</div>
+      </section>
+
+      <section className="flex gap-6 items-center">
+        <div>Média</div>
+        <div>Investisseurs</div>
+        <div>Carrières</div>
+        <div>Partenariat Paris 2024</div>
+        <img src="/SearchIcon.svg" alt="Search Icon" />
+
+        <section>
+          <button className="flex gap-2">
+            <img src="/GlobeIcon.svg" alt="Globe Icon" />
+            <span>Global</span>
+          </button>
+        </section>
+      </section>
+    </nav>
+  );
+};
+export default LargeNavBar;
