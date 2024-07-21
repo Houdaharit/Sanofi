82225ee012035981176981dec63ad79737afc5f9
Houdaharit <houdaharit20@gmail.com>
2024-07-21 23:35:06 +0100

    ADD: communique de presse page

---
+++ src/pages/CommuniqueDePresse.tsx
@@ -0,0 +1,11 @@
+import Main from "../components/CommuniqueDePresse/Main";
+import Layout from "../components/Layout";
+
+const CommuniqueDePresse = () => {
+  return (
+    <Layout>
+      <Main />
+    </Layout>
+  );
+};
+export default CommuniqueDePresse;
