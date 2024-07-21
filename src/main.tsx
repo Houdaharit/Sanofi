2749f7981be57c4b953d92824de568653f566883
Houdaharit <houdaharit20@gmail.com>
2024-07-21 23:36:01 +0100

    ROUTES: added a the path to the communique de presse page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
+import CommuniqueDePresse from "./pages/CommuniqueDePresse.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "media/communique de presse",
+    element: <CommuniqueDePresse />,
   },
 ]);
 
