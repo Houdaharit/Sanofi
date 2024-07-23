3ce3cc9cc158f51a2f696253bd5c25bf78e6a400
Houdaharit <houdaharit20@gmail.com>
2024-07-23 22:22:17 +0100

    ADD: notre entreprise page

---
+++ src/pages/NotreEntreprise.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import Main from "../components/NotreEntreprise/Main";
+
+const NotreEntreprise = () => {
+  return (
+    <Layout>
+      <Main />
+    </Layout>
+  );
+};
+export default NotreEntreprise;
