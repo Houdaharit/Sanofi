f7de972e87337551f990f7700868d4e7e1f60f2d
Houdaharit <houdaharit20@gmail.com>
2024-07-24 02:06:45 +0100

    Iterate over text[] elements

--- src/components/Card/private/ArticleHorizontal.tsx
+++ src/components/Card/private/ArticleHorizontal.tsx
@@ -1,25 +1,32 @@
-import { CardProps } from ".";
+import { CardProps } from "..";
 
 const CardHorizontal = (props: CardProps) => {
+  const paragraphs: string[] = props.text;
   return (
     <div className="flex lg:gap-12 gap-6 overflow-hidden flex-wrap lg:flex-nowrap justify-center">
       <section className="flex flex-col gap-3">
-
         <h3 className="text-3xl leading-[54px] font-bold">{props.title}</h3>
-        
-        <section>
-        <p className="text-base leading-6">{props.text}</p>
+
+        <section className="flex flex-col gap-6">
+          {paragraphs.map((paragraph, index) => (
+            <p className="text-base leading-6" key={index}>
+              {paragraph}
+            </p>
+          ))}
         </section>
 
         <section className="flex gap-3 items-center">
           <div className="font-bold text-base">En Savoir plus </div>
           <img src="/link.svg" className="h-4" />
         </section>
-
       </section>
 
-      <section className={props.order === "first" ? "order-first" : "lg:order-last order-first"}>
-      <img src={props.img} className="lg:w-[684px] lg:max-w-none w-screen" />
+      <section
+        className={
+          props.order === "first" ? "order-first" : "lg:order-last order-first"
+        }
+      >
+        <img src={props.img} className="lg:w-[684px] lg:max-w-none w-screen" />
       </section>
     </div>
   );
