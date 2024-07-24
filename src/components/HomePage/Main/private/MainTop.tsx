ed1d266b6980cc07586b6595737306b97913d6b4
Houdaharit <houdaharit20@gmail.com>
2024-07-24 02:08:03 +0100

    STYLE: some style changes

--- src/components/HomePage/Main/private/MainTop.tsx
+++ src/components/HomePage/Main/private/MainTop.tsx
@@ -6,7 +6,7 @@ const MainTop = () => {
   const {boxHeight} = useContextBox();
   return (
     <div className="relative" style={{marginBottom: boxHeight}}>
-      <section className="absolute lg:m-16 mt-[250px] m-4">
+      <section className="absolute lg:m-16 mt-[250px] m-4 lg:max-w-none">
       <ArticleBox />
       </section>
       <img src="/andrea-pompe-patient_20240222140549.png" className="lg:h-[576px] h-[296px] object-cover w-full"/>
